50155ef3214165ed1629c51136cf5b353650a32a
Salvatore Esposito <SalvatoreEsposito@healthireland.onmicrosoft.com>
2025-01-19 20:44:55 +0000

    Update

--- BlazorUI/wwwroot/js/site.js
+++ BlazorUI/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿
+﻿function showSweetAlert(title, message) {
+    return Swal.fire({
+        title: title,
+        text: message,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Ok',
+        cancelButtonText: 'Cancel'
+    }).then((result) => {
+        return result.isConfirmed;
+    });
+}
